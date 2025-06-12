648efe2c033a2dca36f34554ce02f616f5f5dc09
sai kumar lakkimsetti <eswarsaikumar2@gmail.com>
2025-06-12 18:37:30 +0530

    update

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "sg_id" {
+  type        = string
+  default     = "sg-0fb4314a3e3c19b76"
+  description = "description"
+}
+
+variable "public_subnet_id" {
+    type = string
+    default = "subnet-026fa8e1dc737230f"
+}

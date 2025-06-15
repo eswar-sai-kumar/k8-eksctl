2f8b6164f7b990135b1331e33dde5e3b91cc1538
sai kumar lakkimsetti <eswarsaikumar2@gmail.com>
2025-06-15 07:52:56 +0530

    update

--- workstation.tf
+++ workstation.tf
@@ -8,7 +8,6 @@ module "workstation" {
   # convert StringList to list and get first element
   subnet_id = var.public_subnet_id
   ami = data.aws_ami.ami_info.id
-  user_data = file("workstation.sh")
   tags = {
         Name = "workstation"
     }
